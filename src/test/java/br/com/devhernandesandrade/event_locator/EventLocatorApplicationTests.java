6345804158165566ac6094c6f742426d7cacc852
HernandesHD <hernandes.andrade@tamboro.com.br>
2025-07-11 14:16:43 -0300

    feat(api): inicializando o projeto Event Locator

---
+++ src/test/java/br/com/devhernandesandrade/event_locator/EventLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devhernandesandrade.event_locator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLocatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
