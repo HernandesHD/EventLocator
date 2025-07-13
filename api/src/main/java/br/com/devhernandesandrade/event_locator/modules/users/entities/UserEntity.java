e7ff13e7e91bb82bfb2cd1f3cdb9b602b141424f
HernandesHD <hernandes.andrade@tamboro.com.br>
2025-07-13 02:27:43 -0300

    feat(api): terminando configuração da classe UserEntity.

--- api/src/main/java/br/com/devhernandesandrade/event_locator/modules/users/entities/UserEntity.java
+++ api/src/main/java/br/com/devhernandesandrade/event_locator/modules/users/entities/UserEntity.java
@@ -1,19 +1,41 @@
 package br.com.devhernandesandrade.event_locator.modules.users.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@Table
+@Entity(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private RoleUser role;
 
+    @Column(name = "created_at")
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
