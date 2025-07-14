76d028dd4d4b95cff5c39de54dd47cdd6f709e00
HernandesHD <hernandes.andrade@tamboro.com.br>
2025-07-14 00:59:49 -0300

    refactor(api): adiciona metodos GET - list pageable, GET por ID do evento, Delete - SoftDelete e PUT para atualizar dados do evento.

--- api/src/main/java/br/com/devhernandesandrade/event_locator/modules/events/controller/EventController.java
+++ api/src/main/java/br/com/devhernandesandrade/event_locator/modules/events/controller/EventController.java
@@ -2,16 +2,15 @@ package br.com.devhernandesandrade.event_locator.modules.events.controller;
 
 
 import br.com.devhernandesandrade.event_locator.modules.events.dto.CreateEventRequest;
-import br.com.devhernandesandrade.event_locator.modules.events.useCases.CreateEventUseCase;
+import br.com.devhernandesandrade.event_locator.modules.events.dto.UpdateEventRequest;
+import br.com.devhernandesandrade.event_locator.modules.events.useCases.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
@@ -19,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
 
     private final CreateEventUseCase createEventUseCase;
+    private final ListEventUseCase listEventsUseCase;
+    private final GetEventByIdUseCase getEventByIdUseCase;
+    private final UpdateEventUseCase updateEventUseCase;
+    private final SoftDeleteEventUseCase softDeleteEventUseCase;
 
     @PostMapping("/")
     public ResponseEntity<?> create(HttpServletRequest request, @RequestBody @Valid CreateEventRequest eventRequest) {
@@ -29,6 +32,29 @@ public class EventController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> list(Pageable pageable) {
+        return ResponseEntity.ok(listEventsUseCase.execute(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable Long id) {
+        return ResponseEntity.ok(getEventByIdUseCase.execute(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id,
+                                    @Valid @RequestBody UpdateEventRequest request) {
+        updateEventUseCase.execute(id, request);
+        return ResponseEntity.ok("Evento atualizado");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        softDeleteEventUseCase.execute(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
